9c24655caef73e8367bb99b67be2e65dd2ead45d
Trijeet Modak <uniquetrij@gmail.com>
2020-01-30 18:42:33 +0530

    initial commit

---
+++ src/main/java/org/jhipster/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package org.jhipster.repository;
+
+import org.jhipster.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
